5ea5570720fd03fe88ba8d1ddd4796b780b97581
Jairo Rivera <jariver92@gmail.com>
2020-10-10 18:46:48 +0200

    capture info title and content

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,15 +1,32 @@
 import Layout from "../../components/Layout";
 import dynamic from "next/dynamic";
+import { useState,useEffect } from 'react';
 import "react-quill/dist/quill.snow.css";
 
+//dynamically importing react-quill
+const ReactQuill = dynamic(import("react-quill"), {
+  ssr: false,
+  loading: () => <p>Loading ...</p>,
+});
+
+
+
 const Notes = () => {
-  const ReactQuill = dynamic(import("react-quill"), {
-    ssr: false,
-    loading: () => <p>Loading ...</p>,
-  });
+  const [content, setContent] = useState('');
+  const [title, setTitle]=useState('');
+  // def function onChange
+const onChange = (e) => {
+  setTitle(e.target.value)
+}
   return (
     <Layout>
-      <ReactQuill theme="snow" />
+      <from onSubmit='add_method'>
+        <label>Titulo:</label>
+        <input type='text' name='title' value={title} onChange={onChange}/>
+      <ReactQuill theme="snow" value={content} onChange={setContent}/>
+      <button type='submit'>Guardar</button>
+      {console.log(content, title)}
+      </from>
     </Layout>
   );
 };
