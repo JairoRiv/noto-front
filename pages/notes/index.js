b755c110add257330783c2f2cf55017294ee181b
jairo rivera <jariver92@gmail.com>
2020-10-11 14:33:55 +0200

    add post method

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,7 +1,9 @@
 import Layout from "../../components/Layout";
 import dynamic from "next/dynamic";
-import { useState,useEffect } from 'react';
+import { useState } from "react";
 import "react-quill/dist/quill.snow.css";
+import Router from "next/router";
+import axios from "axios";
 
 //dynamically importing react-quill
 const ReactQuill = dynamic(import("react-quill"), {
@@ -9,24 +11,47 @@ const ReactQuill = dynamic(import("react-quill"), {
   loading: () => <p>Loading ...</p>,
 });
 
-
-
 const Notes = () => {
-  const [content, setContent] = useState('');
-  const [title, setTitle]=useState('');
+  const [content, setContent] = useState("");
+  const [title, setTitle] = useState("");
+
   // def function onChange
-const onChange = (e) => {
-  setTitle(e.target.value)
-}
+  const onChange = (e) => {
+    e.preventDefault();
+    setTitle(e.target.value);
+  };
+
+  //def function submitData
+  const submitData = (e) => {
+    e.preventDefault();
+    const data = {
+      title: title,
+      content: content,
+      email: "prueba@test.com",
+    };
+
+    axios
+      .post("https://notoapi.herokuapp.com/api/notes", data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <Layout>
-      <from onSubmit='add_method'>
-        <label>Titulo:</label>
-        <input type='text' name='title' value={title} onChange={onChange}/>
-      <ReactQuill theme="snow" value={content} onChange={setContent}/>
-      <button type='submit'>Guardar</button>
-      {console.log(content, title)}
-      </from>
+      <div>
+        <form>
+          <label>Titulo:</label>
+          <input type="text" name="title" value={title} onChange={onChange} />
+          <ReactQuill theme="snow" value={content} onChange={setContent} />
+          <button type="submit" onClick={submitData}>
+            Guardar
+          </button>
+        </form>
+      </div>
     </Layout>
   );
 };
